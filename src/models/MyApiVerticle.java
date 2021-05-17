af66bec801ebef0094438339cfbff5f3d849b1d0
pourmera.badiane <pourmera.badiane@finditech.com>
2021-05-17 02:26:36 +0200

    add last configuration of api

--- src/models/MyApiVerticle.java
+++ src/models/MyApiVerticle.java
@@ -10,7 +10,14 @@ import io.vertx.ext.web.Router;
         private static final Logger LOGGER = LoggerFactory.getLogger(MyApiVerticle.class);
         @Override
         public void start() throws Exception {
-
+            LOGGER.info("Dans le start...");
+            final Router router = Router.router(vertx);
+            final ApiResource apiResource = new ApiResource();
+            final Router apiSubRouter = apiResource.getSubRouter(vertx);
+            router.mountSubRouter("/api/v1/communique", apiSubRouter);
+            vertx.createHttpServer()
+                    .requestHandler(router::accept)
+                    .listen(9990);
         }
         @Override
         public void stop() throws Exception {
