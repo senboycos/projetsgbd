b2f5a6f2728888f731678e591999d61a25587e6f
fatoukeita <fatoukeita040@gmail.com>
2021-06-07 00:36:39 +0000

    acquisition du fichier

---
+++ src/interface/Aqcuisition.py
@@ -0,0 +1,34 @@
+from tkinter import *
+from subprocess import call
+
+window = Tk()
+
+pyprog = 'importation.py'
+
+
+def callpy(): call(['python', pyprog])
+
+
+window.title("Covid19")
+
+window.geometry("1080x720")
+window.minsize(480, 360)
+window.iconbitmap("")
+window.configure(background='#FAF3CA')
+
+frame = Frame(window, bg='#FAF3CA')
+frame_buttons = Frame(window, bg='#FAF3CA')
+
+label_title = Label(frame, text="Data Aqcuisition", font=("Montserrat", 40), bg='#FAF3CA', fg="Black")
+label_title.pack()
+
+download_button = Button(frame_buttons, text="Telecharger le fichier du jour", font=("Montserrat", 25), bg='white', fg='Black', command=callpy)
+download_button.pack()
+insert_button = Button(frame_buttons, text="Mettre à jour les fichiers", font=("Montserrat", 25), bg='white', fg='Black',)
+insert_button.pack()
+
+frame.pack()
+frame_buttons.pack()
+
+window.mainloop()
+
